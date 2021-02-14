ffbededa29ecab7a5abded6cc5729c32914bc9e7
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2021-02-14 11:30:08 -0500

    [server] 1) Passable base Docker image, 2) Remove hardcoded git repos to clone. Dedicate to .repos file on ARIAC repo. 3) Docker tag with org name

--- ariac-server/build-images.sh
+++ ariac-server/build-images.sh
@@ -16,7 +16,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "${YELLOW}Build image: ariac-server${NOCOLOR}"
 
 UBUNTU_DISTRO=${1:-bionic}
-ROSDISTRO=${2:-melodic}
+ROSDISTRO=melodic
 case $UBUNTU_DISTRO in
   "bionic")
     ROSDISTRO="melodic"
@@ -31,10 +31,12 @@ case $UBUNTU_DISTRO in
     ;;
 esac
 
+BASE_DIMG=${2:-ros:${ROSDISTRO}-ros-base}
+
 DOCKER_ARGS="--no-cache"
 USERID=`id -u $USER`
 if [[ ${USERID} != 0 ]]; then
   DOCKER_ARGS="--build-arg USERID=${USERID}"
 fi
 
-docker build --force-rm ${DOCKER_ARGS} --build-arg UBUNTU_DISTRO=$UBUNTU_DISTRO --build-arg ROSDISTRO=$ROSDISTRO --tag ariac-server-$ROSDISTRO:latest $DIR/ariac-server
+docker build --force-rm ${DOCKER_ARGS} --build-arg BASE_DIMG="${BASE_DIMG}" --build-arg UBUNTU_DISTRO=$UBUNTU_DISTRO --build-arg ROSDISTRO=$ROSDISTRO --tag usnistgov/ariac:server-$UBUNTU_DISTRO $DIR/ariac-server
